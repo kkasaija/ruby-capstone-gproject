a348b1e20213d7c6a3d9d14af18cd7dde3e5f945
Kasaija Kenneth <kasaijak@gmail.com>
2023-01-19 12:59:32 +0300

    Implement add_book method in book_operations class

--- book_operations.rb
+++ book_operations.rb
@@ -7,4 +7,20 @@ class BookOperations
     @books = []
     @labels =[]
   end
+  
+    # Function to add Book
+  
+    def add_book
+      print 'Enter book name: '
+      name = gets.chomp
+      print 'Enter book publisher: '
+      publisher = gets.chomp
+      print 'Enter publish date: '
+      publish_date = gets.chomp
+      print 'Enter cover state cover state (good/bad): '
+      cover_state = gets.chomp
+      book = Book.new(name: name, cover_state: cover_state, publisher: publisher, publish_date: publish_date)
+      @books << book
+      puts 'Book added successfully'
+    end
 end
