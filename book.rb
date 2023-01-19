ddd2c729a53de440b4a8da48a4b7ce25206abf06
Kasaija Kenneth <kasaijak@gmail.com>
2023-01-19 12:52:48 +0300

    Implement the can_be_archived method in book class

--- book.rb
+++ book.rb
@@ -7,4 +7,10 @@ class Book < Item
     @publisher = publisher
     @cover_state = cover_state
   end
+
+  def can_be_archived?
+    super || @cover_state == 'bad'
+  end
+  
+  private :can_be_archived?
 end
