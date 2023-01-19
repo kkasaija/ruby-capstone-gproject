b0c96558e726882255525581f1c331ca3c773377
Kasaija Kenneth <kasaijak@gmail.com>
2023-01-19 21:05:47 +0300

    Instantiate music object in app class

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require './book_operations'
+require './music_operations'
 class App
   def console_entry_point
     puts ''
@@ -18,7 +19,7 @@ class App
     puts('-------------------------------')
     puts '1 - List all books'
     puts '2 - List all music albums'
-    puts '3 - List of games'
+    puts '3 - List all games'
     puts '4 - List all genres'
     puts '5 - List all labels'
     puts '6 - List all authors'
@@ -32,15 +33,18 @@ class App
 
   def option(input)
     operation = BookOperations.new
+    music = MusicOperations.new
     case input
     when '1'
       operation.list_all_books
     when '2'
       # list all music albums
+      music.list_all_albums
     when '3'
       # list_of_games
     when '4'
       # list all genres
+      music.list_all_genres
     when '5'
       operation.list_all_labels
     when '6'
@@ -50,11 +54,13 @@ class App
 
   def extras(option)
     operation = BookOperations.new
+    music = MusicOperations.new
     case option
     when '7'
       operation.add_book
     when '8'
       # add_album
+      music.add_music_album
     when '9'
       # add_game
     else
