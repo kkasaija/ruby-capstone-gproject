aacb8136887103f821565138010bfe6f8cd49c33
masouma <masouma2020@gmail.com>
2023-01-20 11:20:18 +0430

    Initialize class and archive method

---
+++ music.rb
@@ -0,0 +1,16 @@
+require './item'
+
+class Music < Item
+  attr_accessor :publish_date, :on_spotify, :archived
+
+  def initialize(publish_date:, on_spotify: true, name: 'unknown', archived: false)
+    super(publish_date: publish_date, name: name, archived: archived)
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    super && on_spotify
+  end
+
+  private :can_be_archived?
+end
